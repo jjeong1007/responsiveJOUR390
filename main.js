77dd89159efe3a7d65751adbb0da9e9d61699bdb
jjeong1007 <jeongjason5914@gmail.com>
2025-02-17 16:12:06 -0800

    added main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+const button = document.querySelector("#directory");
+const directory = document.querySelector("#colorDirectory");
+
+let buttonVision = false;
+
+button.addEventListener("click", function() {
+    if (!buttonVision) {
+        document.getElementById("colorDirectory").style.opacity = 1;
+        buttonVision = true;
+    } else if (buttonVision) {
+        document.getElementById("colorDirectory").style.opacity = 0;
+        buttonVision = false;
+    }
+});
+
